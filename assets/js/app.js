55cfc3a1fe14a6660ce88aa0b3e743380f761d4d
Aslbek <aslbekcoder009@gmail.com>
2024-09-14 15:18:44 +0500

    Js go

---
+++ assets/js/app.js
@@ -0,0 +1,26 @@
+var ism ="";
+console.log("Hello world"+ism);
+
+console.log("My name is Aslbek"+ism);
+
+console.log("My surname is Abdisalomov"+ism);
+
+console.log("I live in Khushali village"+ism);
+
+console.log("My hobby is coding"+ism);
+
+console.log("We are 5 people in the family"+ism);
+
+console.log("I am the eldest child in the family"+ism);
+
+console.log("I want to become a web developer in the future"+ism);
+
+console.log("I am currently studying at school, I am a 9th grade student"+ism);
+
+console.log("My school number is 86th"+ism);
+
+console.log("I`m 15 years old"+ism);
+
+console.log("Bye bye"+ism);
+
+
